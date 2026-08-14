9336c0c5bfcbfde94f9ec70f91b353f5e3ffdb48
Roosevelt Wooden <roosevelt.wooden@students.maestrocollege.edu>
2026-08-14 16:59:19 -0400

    Restore original EBG+ logo in universal branding

--- scripts/apply-universal-branding.mjs
+++ scripts/apply-universal-branding.mjs
@@ -4,8 +4,8 @@ const appPath = new URL('../src/App.tsx', import.meta.url)
 let source = fs.readFileSync(appPath, 'utf8')
 
 source = source
-  .replaceAll('/ebgplus/ebgplus-logo.png', '/ebgplus-logo.svg')
-  .replaceAll('/ebgplus-logo.png', '/ebgplus-logo.svg')
+  .replaceAll('/ebgplus/ebgplus-logo.png', '/ebgplus-logo.png')
+  .replaceAll('/ebgplus-logo.svg', '/ebgplus-logo.png')
 
 fs.writeFileSync(appPath, source)
-console.log('Applied universal transparent EBG+ branding.')
+console.log('Applied universal EBG+ branding with the original logo asset.')
