32220165ebcb881ed4ba71539f50362fcdeafa7c
Roosevelt Wooden <roosevelt.wooden@students.maestrocollege.edu>
2026-08-13 21:23:44 -0400

    Load Phase 1.7 sizing fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './phase17-fixes.css'
 import App from './App.tsx'
 import SplashScreen from './components/SplashScreen.tsx'
 
